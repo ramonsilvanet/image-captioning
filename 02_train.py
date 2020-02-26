301e046ed83231c14878e77dabbe6c122405e174
Ramon Ferreira Silva <ramon.silva@resultadosdigitais.com.br>
2020-02-26 13:12:23 -0300

    :tada: model with tensorflow

---
+++ 02_train.py
@@ -0,0 +1,227 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+# You'll generate plots of attention in order to see which parts of an image
+# our model focuses on during captioning
+import matplotlib.pyplot as plt
+
+# Scikit-learn includes many helpful utilities
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+import re
+
+import os
+import time
+import json
+import pickle
+import shutil
+import numpy as np
+from glob import glob
+from PIL import Image
+from tqdm import tqdm
+
+#custom functions
+from utils import *
+from neuralnetworks import CNNEncoder, RNNDecoder
+
+annotation_folder = os.path.abspath('.') + '/datasets/annotations/'
+annotation_file = os.path.join(annotation_folder ,'captions_train2014.json')
+PATH = os.path.abspath('.') + '/datasets/train2014/'
+
+########################################################
+#                    START EXECUTION                   #
+########################################################
+
+print('Start execution')
+
+print('Loading annotations')
+# Read the json file
+with open(annotation_file, 'r') as f:
+    annotations = json.load(f)
+
+# Store captions and image names in vectors
+all_captions = []
+all_img_name_vector = []
+
+for annot in annotations['annotations']:
+    caption = '<start> ' + annot['caption'] + ' <end>'
+    image_id = annot['image_id']
+    full_coco_image_path = PATH + 'COCO_train2014_' + '%012d.jpg' % (image_id)
+
+    all_img_name_vector.append(full_coco_image_path)
+    all_captions.append(caption)
+
+print('shuffling train vectors')
+# Shuffle captions and image_names together
+# Set a random state
+train_captions, img_name_vector = shuffle(all_captions,
+                                          all_img_name_vector,
+                                          random_state=1)
+
+# Select the first 30000 captions from the shuffled set
+num_examples = 3000
+train_captions = train_captions[:num_examples]
+img_name_vector = img_name_vector[:num_examples]
+
+print(len(train_captions), len(all_captions))
+
+print('vectorize images')
+# Load Neural Network Model Inception v3
+image_model = tf.keras.applications.InceptionV3(include_top=False,
+                                                weights='imagenet')
+new_input = image_model.input
+hidden_layer = image_model.layers[-1].output
+
+image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+
+# Get unique images
+encode_train = sorted(set(img_name_vector))
+
+# Feel free to change batch_size according to your system configuration
+image_dataset = tf.data.Dataset.from_tensor_slices(encode_train)
+image_dataset = image_dataset.map(
+  load_image, num_parallel_calls=tf.data.experimental.AUTOTUNE).batch(16)
+
+for img, path in tqdm(image_dataset):
+  batch_features = image_features_extract_model(img)
+  batch_features = tf.reshape(batch_features,
+                              (batch_features.shape[0], -1, batch_features.shape[3]))
+
+  for bf, p in zip(batch_features, path):
+    path_of_feature = p.numpy().decode("utf-8")
+    np.save(path_of_feature, bf.numpy())
+
+# Preprocess and tokenize the captions
+# Choose the top 5000 words from the vocabulary
+top_k = 5000
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
+                                                  oov_token="<unk>",
+                                                  filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+tokenizer.fit_on_texts(train_captions)
+train_seqs = tokenizer.texts_to_sequences(train_captions)
+
+print('Tokenizing words')
+tokenizer.word_index['<pad>'] = 0
+tokenizer.index_word[0] = '<pad>'
+
+# Create the tokenized vectors
+train_seqs = tokenizer.texts_to_sequences(train_captions)
+
+# Pad each vector to the max_length of the captions
+# If you do not provide a max_length value, pad_sequences calculates it automatically
+cap_vector = tf.keras.preprocessing.sequence.pad_sequences(train_seqs, padding='post')
+
+# Calculates the max_length, which is used to store the attention weights
+max_length = calc_max_length(train_seqs)
+
+# Create training and validation sets using an 80-20 split
+img_name_train, img_name_val, cap_train, cap_val = train_test_split(img_name_vector,
+                                                                    cap_vector,
+                                                                    test_size=0.2,
+                                                                    random_state=0)
+
+
+print(len(img_name_train), len(cap_train), len(img_name_val), len(cap_val))
+
+
+print('Creating data for testing')
+
+# Feel free to change these parameters according to your system's configuration
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+embedding_dim = 256
+units = 512
+vocab_size = top_k + 1
+num_steps = len(img_name_train) // BATCH_SIZE
+# Shape of the vector extracted from InceptionV3 is (64, 2048)
+# These two variables represent that vector shape
+features_shape = 2048
+attention_features_shape = 64
+
+dataset = tf.data.Dataset.from_tensor_slices((img_name_train, cap_train))
+
+# Use map to load the numpy files in parallel
+dataset = dataset.map(lambda item1, item2: tf.numpy_function(
+          map_func, [item1, item2], [tf.float32, tf.int32]),
+          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+# Shuffle and batch
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+########################################################
+#                    CREATING MODEL                    #
+########################################################
+print('Creating model')
+encoder = CNNEncoder.CNN_Encoder(embedding_dim)
+decoder = RNNDecoder.RNN_Decoder(embedding_dim, units, vocab_size)
+
+optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+# Checkpoint
+checkpoint_path = os.path.join( os.path.abspath('.') ,"outputs/checkpoints/train")
+ckpt = tf.train.Checkpoint(encoder=encoder,
+                           decoder=decoder,
+                           optimizer = optimizer)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+print('Start training. This can take several time (hours)')
+
+start_epoch = 0
+if ckpt_manager.latest_checkpoint:
+  start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])
+  # restoring the latest checkpoint in checkpoint_path
+  ckpt.restore(ckpt_manager.latest_checkpoint)
+
+loss_plot = []
+
+# WARNING THIS CAN TAKE SEVERAL HOURS !!!
+EPOCHS = 2
+
+for epoch in range(start_epoch, EPOCHS):
+    start = time.time()
+    total_loss = 0
+
+    for (batch, (img_tensor, target)) in enumerate(dataset):
+        batch_loss, t_loss = train_step(img_tensor, target)
+        total_loss += t_loss
+
+        if batch % 100 == 0:
+            print ('Epoch {} Batch {} Loss {:.4f}'.format(
+              epoch + 1, batch, batch_loss.numpy() / int(target.shape[1])))
+    # storing the epoch end loss value to plot later
+    loss_plot.append(total_loss / num_steps)
+
+    if epoch % 5 == 0:
+      ckpt_manager.save()
+
+    print('Epoch {} Loss {:.6f}'.format(epoch + 1,
+                                         total_loss/num_steps))
+    print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+
+print('Training complete.')
+
+## Plotting
+plt.plot(loss_plot)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Loss Plot')
+plt.savefig('outputs/loss.png')
+
+########################################################
+#                     EVALUATE MODEL                   #
+########################################################
+
+print('Evaluate trained model')
+
+# captions on the validation set
+rid = np.random.randint(0, len(img_name_val))
+image = img_name_val[rid]
+real_caption = ' '.join([tokenizer.index_word[i] for i in cap_val[rid] if i not in [0]])
+result, attention_plot = evaluate(image, decoder)
+
+print('Real Caption:', real_caption)
+print('Prediction Caption:', ' '.join(result))
+plot_attention(image, result, attention_plot)
---
+++ 03_evaluate.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from PIL import Image
+from utils import evaluate
+
+
+image_url = 'https://tensorflow.org/images/surf.jpg'
+image_extension = image_url[-4:]
+image_path = tf.keras.utils.get_file('image'+image_extension,
+                                     origin=image_url)
+
+result, attention_plot = evaluate(image_path)
+print ('Prediction Caption:', ' '.join(result))
+plot_attention(image_path, result, attention_plot)
+# opening the image
+Image.open(image_path)
